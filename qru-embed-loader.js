5fffb369a21f81f23d82b0f0d2f99b1b68d3addf
my-qru <yalcin.altay@myqru.de>
2025-04-08 20:53:39 +0200

    Update qru-embed-loader.js
    test without resizer

--- qru-embed-loader.js
+++ qru-embed-loader.js
@@ -7,30 +7,18 @@
     return;
   }
 
-  const resizerScript = document.createElement('script');
-  resizerScript.src = "https://19480076e0a779cef1840e3f73d5ec65.cdn.bubble.io/f1737974580607x535025922703154600/iframeResizer.min.js";
+  const iframe = document.createElement('iframe');
+  iframe.id = "qru-qontent-wall";
+  iframe.src = `https://embed.myqru.de/?wall=${wallId}`;
+  iframe.frameBorder = "0";
+  iframe.scrolling = "no";
+  iframe.width = "100%";
+  iframe.title = "qru-qontent-wall";
+  iframe.setAttribute("allowfullscreen", "");
+  iframe.setAttribute("allow", "fullscreen"); 
+  iframe.style.border = "none";
+  iframe.style.minHeight = "500px";
+  iframe.style.visibility = "visible"; // direkt sichtbar
 
-  resizerScript.onload = function() {
-    const iframe = document.createElement('iframe');
-    iframe.id = "qru-qontent-wall";
-    iframe.src = `https://embed.myqru.de/?wall=${wallId}`;
-    iframe.frameBorder = "0";
-    iframe.scrolling = "no";
-    iframe.width = "100%";
-    iframe.title = "qru-qontent-wall";
-    iframe.setAttribute("allowfullscreen", "");
-    iframe.setAttribute("allow", "fullscreen"); 
-    iframe.style.border = "none";
-    iframe.style.minHeight = "500px";
-    iframe.style.visibility = "hidden"; // Erst anzeigen, wenn geladen
-
-    thisScript.insertAdjacentElement('afterend', iframe);
-
-    iframe.onload = function() {
-      iframe.style.visibility = "visible"; // Sichtbar machen
-      iFrameResize({ log: false, checkOrigin: false }, "#qru-qontent-wall");
-    };
-  };
-
-  document.head.appendChild(resizerScript);
+  thisScript.insertAdjacentElement('afterend', iframe);
 })();
